bd501a85f6e1a9d27c3f0287942374a065fd6d69
Gladstone Rose <gladstonerose@hotmail.com>
2024-09-10 15:21:15 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import { useState } from 'react';
+
+function App() {
+
+const [color, setColor] = useState('Red');
+
+const changeColor = ()=>{
+setColor('Blue')
+
+}
+
+  return (
+    <>
+      <div className="App"><h1>My favourite color is {color}!</h1></div>
+      <button onClick={changeColor}>Blue</button>
+      
+    </>
+  )
+}
+
+export default App
