f6189ecf0ad1f7a8d9f2fc7a855f507926a296e2
Gladstone Rose <gladstonerose@hotmail.com>
2024-09-11 23:56:53 -0500

    modify package.json files

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/explained-in-2hours/',  // Add this line
   plugins: [react()],
 })
